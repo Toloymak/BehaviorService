b6041fa75188e69a805d13ed8ee8383a24106aed
Alexey <brovko-a@live.com>
2019-11-23 21:05:15 +0500

    Create Dao

---
+++ Dao/Entities/Person.cs
@@ -0,0 +1,10 @@
+﻿
+namespace Dao.Entities
+{
+    public class Person : Entity
+    {
+        public string Fio { get; set; }
+        public int Age { get; set; }
+        public VerdictType Verdict { get; set; }
+    }
+}
---
+++ Dao/Structs/VerdictType.cs
@@ -0,0 +1,8 @@
+﻿namespace Dao.Entities
+{
+    public enum VerdictType
+    {
+        Good = 0,
+        Bad = 1
+    }
+}

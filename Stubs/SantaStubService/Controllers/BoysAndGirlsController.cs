01af9b68f21388d40b417300aff0d89052c2b9cd
Alexey <brovko-a@live.com>
2019-11-23 14:57:34 +0500

    Stub Service for fast integration testing

---
+++ Stubs/SantaStubService/Controllers/BoysAndGirlsController.cs
@@ -0,0 +1,24 @@
+﻿using Dao.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Proxies.Models;
+
+namespace SantaStubService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BoysAndGirlsController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get(string fio, int age)
+        {
+            var verdictDto = new VerdictDto
+            {
+                Fio = fio,
+                Age = age,
+                Verdict = VerdictType.Good
+            };
+
+            return Ok(verdictDto);
+        }
+    }
+}
